57f69ff90496fe44e20bcdff5c48a06a03b23af2
Anneli Bentler <annelibentler@hotmail.com>
2022-04-14 18:58:49 +0200

    yepl not finished yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurants = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_name, category: 'japanese', phone_number: Faker::PhoneNumber.cell_phone)
+end
+
+5.times do
+  restaurants = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_name, category: 'chinese', phone_number: Faker::PhoneNumber.cell_phone)
+end
